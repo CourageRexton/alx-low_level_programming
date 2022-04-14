75b7d311f4dffc26ed758cf6707bd3e8e8a83126
CourageRexton <courageeboigbe20@gmail.com>
2022-04-14 21:18:37 +0000

    C - more pointers, arrays and string

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,32 +2,25 @@
 
 /**
  * main - finds and prints the largest prime factor of the number 612852475143
- * followed by a new line
- * Return: Always 0 (Success)
+ *
+ * Return: int
  */
+
 int main(void)
 {
-	long long int x, i, pf;
+unsigned long n, i, max;
 
-	// pf = -1;
-	x = 612852475143;
+n = 612852475143;
+max = 1;
 
-	while (x % 2 == 0)
-	{
-		pf = 2;
-		x = x / 2;
-	}
-	for (i = 3; i <= (x / 2); i = i + 2)
-	{
-		while (x % i == 0)
-		{
-			pf = i;
-			x = x / i;
-		}
-	}
-	if (x > 2)
-		pf = x;
-
-	printf("%ld\n", pf);
-	return (0);
+for (i = 3; i <= n; i +=2)
+{
+while (n % i == 0)
+{
+max = n;
+n = n / i;
+}
+}
+printf("%lu\n", max);
+return (0);
 }

d9638408ae9f48b62e760c7501dad7e1f7cae46c
CourageRexton <courageeboigbe20@gmail.com>
2022-04-14 20:22:26 +0000

    Added return statement in Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz Test
+ *
+ * Description: prints the numbers from 1 to 100, followed by a new line
+ * multiples of 3 and 5 are replaced with Fizz and Buzz respectively
+ * multiples of both 3 and 5 are replaced with FizzBuzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 != 0))
+			printf("Fizz ");
+		else if ((i % 3 != 0) && (i % 5 == 0))
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz");
+	putchar('\n');
+	return (0);
+}

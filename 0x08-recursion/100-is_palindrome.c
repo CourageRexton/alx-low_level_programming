dde501753497f7b9c306fbec97abb50f032776dd
CourageRexton <courageeboigbe20@gmail.com>
2022-04-29 00:36:40 +0000

    Advanced Task 1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,47 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _myStrlen - gets string length
- * @p: string to be counted
- * Return: returns string length
- */
-char _myStrlen(char *p)
-{
-	if (!*p)
-		return (0);
-	return (1 + _myStrlen(p + 1));
-}
-/**
- * p1 - string iterator
- * @s: string paramsss
- * @l: length of string param
- * Return: returns string length
- */
-int p1(char *s, int l)
-{
-	if (l < 1)
-		return (1);
-	if (*s == *(s + l))
-		return (p1(s + 1, l - 2));
-	return (0);
-}
-/**
- * is_palindrome - checks if a string is palindrome
- * @s: string to be checked
- * Return: 1 or 0 if palindrome or not palindrome respectively
+ * is_palindrome - Function that returns if the string is a palindrome
+ * @s: Pointer to a string
+ *
+ * Return: 1 - Palindrome | 0 - Not palindrome.
  */
 int is_palindrome(char *s)
 {
-	int len;
-
-	len = _myStrlen(s);
-	return (p1(s, len - 1));
+	return (get_palindrome(s, 0, strlengr(s, 0) - 1, strlengr(s, 0)));
 }
+/**
+ * get_palindrome - Function that returns if the string is a palindrome
+ * @s: Pointer to a string
+ * @i: Iterator
+ * @j: Iterator
+ * @k: Length of the string
+ *
+ * Return: 1 - Palindrome | 0 - Not palindrome.
+ */
+int get_palindrome(char *s, int i, int j, int k)
+{
+	if (k == 0)
+		return (1);
+	if (*(s + i) == *(s + j))
+	{
+		if (j == 0)
+			return (1);
+		else
+			return (get_palindrome(s, ++i, --j, k));
+	}
+	return (0);
+}
+
+/**
+ * strlengr - Function that gets the length of a string.
+ * @a: Pointer to a string
+ * @i: Iterator
+ *
+ * Return: Length of the string (int)
+ */
+int strlengr(char *a, int i)
+{
+	if (a[i] != '\0')
+		return (strlengr(a, ++i));
+	return (i);
+}}

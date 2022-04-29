15bc927ece9b6c43f48135d5d10766074f32bfbf
CourageRexton <courageeboigbe20@gmail.com>
2022-04-29 00:45:28 +0000

    C - Recursion

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char);
+int strleng(char *);
+int strlengr(char *, int);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
+int is_palindrome(char *);
+int get_sqrt(int, int);
+int get_prime(int, int);
+int get_palindrome(char *, int, int, int);
+
+
+#endif /* HOLBERTON_H */

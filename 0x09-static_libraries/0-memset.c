8b0d647caf480ada38985c453f254d9955d527b4
Courage Rexton <courageeboigbe20@gmail.com>
2022-05-03 23:56:54 +0100

    C - Static Libraries

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,11 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * _memset - Function that fills memory with a constant byte.
- * @s: Pointer to the memory area
- * @b: Value to replace
- * @n: Number of bytes
+ * _memset - fills the first n bytes of the memory area pointed
+ * ponted to by s with the constant byte b
  *
- * Return: Pointer to the string modified.
+ * @s: pointer to memory area
+ * @b: constant byte
+ * @n: number of byte(s) to be filled
+ *
+ * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -14,9 +16,6 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
-		if (*(s + i) == '\0')
-			break;
 	}
-
 	return (s);
 }
